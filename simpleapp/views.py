53d0991a14d13399bec8b5b9f940e2a6021a2bb7
SerNik4er <106673431+SerNik4er@users.noreply.github.com>
2023-03-02 06:29:40 +0300

    Add files via upload

---
+++ simpleapp/views.py
@@ -0,0 +1,10 @@
+from django.views.generic import ListView
+from .models import Product
+
+
+class ProductsList(ListView):
+    model = Product
+    ordering = 'name'
+    template_name = 'products.html'
+    context_object_name = 'products'
+# Create your views here.
